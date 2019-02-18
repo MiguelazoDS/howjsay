76b30e41d125cc11a289ed736636eb9214c0b57b
Cazajous Miguel A <migue7490@gmail.com>
2019-02-18 11:41:28 -0300

    Revised with perlcritic

--- download_audios.pl
+++ download_audios.pl
@@ -1,26 +1,27 @@
 #!/usr/bin/env perl
 
 use LWP::Simple;
+use strict;
 
-$url="https://howjsay.com/";
-$url_mp3=$url."mp3";
-$contenido=get($url);
+my $url="https://howjsay.com/";
+my $url_mp3=$url."mp3";
+my $contenido=get($url);
 
 system ("if [ ! -e audios ]; then mkdir audios; fi");
-@palabras = $contenido =~ /loadEcho'>(\w+)/g;
+my @palabras = $contenido =~ /loadEcho'>(\w+)/g;
 
 print scalar@palabras;
-@letras = (a..z);
+my @letras = ("a".."z");
 
-foreach $x (@letras){
+foreach my $x (@letras){
     my $url_letra="$url$x";
-    $contenido=get($url_letra);
-    @paginas = $contenido =~ /href='($x\?page=\d+)'>/g;
-    foreach $y (@paginas){
+    my $contenido=get($url_letra);
+    my @paginas = $contenido =~ /href='($x\?page=\d+)'>/g;
+    foreach my $y (@paginas){
         my $url_pagina="$url$y";
         $contenido=get($url_pagina);
         @palabras = $contenido =~ /loadEcho'>(\w+)/g;
-        foreach $z (@palabras){
+        foreach my $z (@palabras){
             my $url_final="$url_mp3/$z.mp3";
             print $url_final, "\n";
 	    system ("wget $url_final -P audios");
